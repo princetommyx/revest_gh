6db5ab83d04bf600d322e066d9ac5380af9d6bf1
princetommyx <lyonlee350@gmail.com>
2025-12-26 04:10:28 -0800

    Fix IndentationError in users/urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import (
     RegisterView, AdminRegisterView, UserDetailView, UpdateLocationView, 
     PasswordResetRequestView, PasswordResetConfirmView, CustomTokenObtainPairView, 
     DebugEmailView, EmailHealthCheckView, GoogleLoginView
